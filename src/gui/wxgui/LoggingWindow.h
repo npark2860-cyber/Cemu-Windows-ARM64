f134405b5e725debc76a360f7916ac0a5a3ca7a5
npark2860-cyber <npark2860@gmail.com>
2026-09-15 22:54:14 +0900

    diag: add haptic state logger controls

--- src/gui/wxgui/LoggingWindow.h
+++ src/gui/wxgui/LoggingWindow.h
@@ -7,7 +7,16 @@
 #include <wx/combobox.h>
 #include "wxgui/components/wxLogCtrl.h"
 
+#include <chrono>
+#include <cstdint>
+#include <iosfwd>
+#include <memory>
+#include <string>
+
 class wxLogEvent;
+class wxTimer;
+class wxTimerEvent;
+class wxStaticText;
 
 class LoggingWindow : public wxFrame, public LoggingCallbacks
 {
@@ -23,7 +32,22 @@ class LoggingWindow : public wxFrame, public LoggingCallbacks
 	void OnFilterChange(wxCommandEvent& event);
 	void OnFilterMessageChange(wxCommandEvent& event);
 
+	void StartHapticRecording();
+	void StopHapticRecording();
+	void SetHapticMarker(std::string marker);
+	void OnHapticTimer(wxTimerEvent& event);
+	void WriteHapticSample();
+	void UpdateHapticStatus(std::string_view message);
+
 	wxComboBox* m_filter;
 	wxLogCtrl* m_log_list;
 	wxCheckBox* m_filter_message;
+
+	std::unique_ptr<wxTimer> m_haptic_timer;
+	std::unique_ptr<std::ofstream> m_haptic_csv;
+	wxStaticText* m_haptic_status{};
+	std::chrono::steady_clock::time_point m_haptic_start{};
+	std::string m_haptic_marker{"Idle"};
+	std::string m_haptic_output_path;
+	uint64_t m_haptic_sample_index{};
 };
